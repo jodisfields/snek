46233c4b005472622af59a97aaaaedc4824f18bf
Jodis Fields <jodis.fields@outlook.com>
2023-04-04 20:41:57 +1000

    added sub pallettes net and config

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,14 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2023 Jodis Fields
 */
 package cmd
 
 import (
 	"os"
 
+	"github.com/jodisfields/snek/cmd/net"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -36,6 +34,9 @@ func Execute() {
 	}
 }
 
+func addSubcommandPaettes() {
+	rootCmd.AddCommand(net.NetCmd)
+}
 func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
@@ -46,6 +47,5 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	addSubcommandPaettes()
 }
-
-
